cb1c3f1166600d54d866bc352cf477c510082ca0
Gutemberg <gutemberg.jhonata@gmail.com>
2022-06-21 08:03:08 -0300

    Implementing changeIcon function

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
+        // Altera seleção
         cleanButtonsAtrributes();
         const button = e.target;
         button.setAttribute('active', 'true');
+
         const id = button.getAttribute('id');
         changeTheme(id);
         changeTitle(id);
+        changeIcon(id);
     })
 })
 
@@ -39,4 +42,19 @@ const titles = {
 function changeTitle(id) {
     const title = titles[id];
     titleElement.innerHTML = title;
+}
+
+const icon = document.querySelector('link[rel="shortcut icon"]');
+const icon32x32 = document.querySelector('link[rel="shortcut icon"][sizes="32x32"]');
+const icon16x16 = document.querySelector('link[rel="shortcut icon"][sizes="16x16"]');
+const iconTypes = {
+    "pomodoro": "",
+    "short-break": "-green",
+    "long-break": "-blue"
+}
+
+function changeIcon(id) {
+    const iconType = iconTypes[id];
+    const href = `./public/favicon${iconType}.ico`;
+    icon.setAttribute('href', href);
 }
