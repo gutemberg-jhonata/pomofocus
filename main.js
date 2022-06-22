30a2205db487c39fbe3ffaa09bd5d39b60d3c57d
Gutemberg <gutemberg.jhonata@gmail.com>
2022-06-22 08:52:47 -0300

    Refactoring changeTheme with theme.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { changeTheme } from "./src/styles/theme.js";
+
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
@@ -10,7 +12,6 @@ buttons.forEach(button => {
         const id = button.getAttribute('id');
         changeTheme(id);
         changeTitle(id);
-        changeIcon(id);
     })
 })
 
@@ -18,18 +19,6 @@ function cleanButtonsAtrributes() {
     buttons.forEach(button => {
         button.setAttribute('active', 'false');
     });
-}
-
-const root = document.documentElement;
-const colors = {
-    'pomodoro': 'rgb(217, 85, 80)',
-    'short-break': 'rgb(76, 145, 149)',
-    'long-break': 'rgb(69, 124, 163)',
-}
-
-function changeTheme(id) {
-    const color = colors[id];
-    root.style.setProperty('--main-color', color);
 }
 
 const titleElement = document.querySelector('title');
@@ -42,19 +31,4 @@ const titles = {
 function changeTitle(id) {
     const title = titles[id];
     titleElement.innerHTML = title;
-}
-
-const icon = document.querySelector('link[rel="shortcut icon"]');
-const icon32x32 = document.querySelector('link[rel="shortcut icon"][sizes="32x32"]');
-const icon16x16 = document.querySelector('link[rel="shortcut icon"][sizes="16x16"]');
-const iconTypes = {
-    "pomodoro": "",
-    "short-break": "-green",
-    "long-break": "-blue"
-}
-
-function changeIcon(id) {
-    const iconType = iconTypes[id];
-    const href = `./public/favicon${iconType}.ico`;
-    icon.setAttribute('href', href);
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,29 @@
+const root = document.documentElement;
+const icon = document.querySelector('link[rel="shortcut icon"]');
+const icon32x32 = document.querySelector('link[rel="shortcut icon"][sizes="32x32"]');
+const icon16x16 = document.querySelector('link[rel="shortcut icon"][sizes="16x16"]');
+
+const themes = {
+    'pomodoro': {
+        color: 'rgb(217, 85, 80)',
+        iconType: ''
+    },
+    'short-break': {
+        color: 'rgb(76, 145, 149)',
+        iconType: '-green'
+    },
+    'long-break': {
+        color: 'rgb(69, 124, 163)',
+        iconType: '-blue'
+    },
+}
+
+export function changeTheme(theme) {
+    const { color, iconType } = themes[theme];
+    const href = `./public/favicon${iconType}.ico`;
+
+    root.style.setProperty('--main-color', color);
+    icon.setAttribute('href', href);
+    icon32x32.setAttribute('href', href);
+    icon16x16.setAttribute('href', href);
+}
